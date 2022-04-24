6a6775b9ad6eadbc5797226c99b538a221549514
EsprIx <3sprix.exe@gmail.com>
2022-04-24 18:53:53 +0200

    added endpoints

---
+++ db/api.py
@@ -0,0 +1,111 @@
+import datetime
+import jwt
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+
+def connect(host="localhost", port=27017) -> dict:
+    """
+    Creates the connection to the database
+    """
+    try:
+        conn = MongoClient(host, port)
+        print("Connected successfully to MongoDB!")
+        return conn
+    except:
+        raise Exception("Could not connect to MongoDB...")
+
+
+def user_exists(db, email) -> bool:
+    """
+    Checks if a user already exists in the database
+    :param db: The database instance.
+    :param email: The email of the user.
+    """
+    query = {"email": email}
+    res = db.users.find_one(query)
+
+    if res:
+        return True
+    else:
+        return False
+
+
+def add_user(db, userData) -> int:
+    """
+    Adds a user to the database
+    :param db: The database instance.
+    :param userData: The informations about the user (email, username, password).
+    """
+    if not user_exists(db, userData["email"]):
+        ts = datetime.datetime.now().timestamp()
+        db.users.insert_one(
+            {
+                **userData,
+                "created_at": ts,
+                "data": {
+                    "budget": 0,
+                    "payments": {"recurrent": [], "single": []},
+                },
+            }
+        )
+
+        token = jwt.encode(
+            {"email": userData["email"], "password": userData["password"]}, "Budget@101#SECRET", algorithm="HS256"
+        )
+
+        return {"status": 200, "token": token, "message": "Successfully added user to the database."}
+    else:
+        return {"status": 409, "message": "User already exists."}
+
+
+def get_token(db, userData) -> dict:
+    """
+    Retrieves a user infos
+    :param db: The database instance.
+    :param userData: The informations about the user (email, username, password).
+    """
+    query = {"email": userData["email"], "password": userData["password"]}
+    res = db.users.find_one(query)
+
+    token = jwt.encode(
+        {"email": userData["email"], "password": userData["password"]}, "Budget@101#SECRET", algorithm="HS256"
+    )
+
+    if res:
+        return {"status": 200, "token": token}
+    else:
+        return {"status": 409, "message": "Either the user doesn't exist, or the credentials provided are not valid."}
+
+
+def get_user(db, userData) -> dict:
+    """
+    Retrieves a user infos
+    :param db: The database instance.
+    :param userData: The informations about the user (email, username, password).
+    """
+    query = {"email": userData["email"]}
+    res = db.users.find_one(query)
+
+    if res:
+        return {"status": 200, "data": res}
+    else:
+        return {"status": 409, "message": "The user does not exist."}
+
+
+def update_user(db, userData) -> None:
+    """
+    Update guild infos in the database
+    :param col: The database collection.
+    :param userData: The informations about the user (email, username, password).
+    """
+    query = {"email": userData["email"]}
+
+    if user_exists(db, userData["email"]):
+        ts = datetime.datetime.now().timestamp()
+        userData.pop("_id")
+        updatedGuildData = {**userData, "updated_at": ts}
+        db.users.replace_one(query, updatedGuildData)
+        return {"status": 200, "message": "Successfully updated the user in the database."}
+    else:
+        return {"status": 409, "message": "The user does not exist."}
---
+++ server.py
@@ -0,0 +1,72 @@
+import repackage
+
+from db.api import add_user, connect, get_user, update_user, get_token
+from utils.parser import parse_json
+
+repackage.up()
+
+import jwt
+
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+conn = connect(host="localhost", port=27017)
+db = conn.budget101
+
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    return "Backend is running!"
+
+
+@app.route("/login", methods=["POST"])
+def login() -> dict:
+    data = request.get_json()
+
+    res = get_token(db, data)
+
+    return res
+
+
+@app.route("/register", methods=["POST"])
+def register() -> dict:
+    data = request.get_json()
+
+    res = add_user(db, data)
+
+    return res
+
+
+@app.route("/getUser", methods=["POST"])
+def getUser() -> dict:
+    data = request.get_json()
+
+    try:
+        decodedToken = jwt.decode(data["token"].replace('"', ""), "Budget@101#SECRET", algorithms=["HS256"])
+        userData = get_user(db, decodedToken)
+
+        if userData["status"] == 200:
+            return {"status": 200, "data": parse_json(userData["data"]), "message": "Token successfully decoded."}
+        else:
+            return {
+                "status": 409,
+                "message": "Token was successfully decoded but user cannot be found in the database.",
+            }
+    except Exception:
+        return {"status": 409, "message": "Token couldn't be decoded."}
+
+
+@app.route("/updateUserInfos", methods=["POST"])
+def updateUserInfos() -> dict:
+    data = request.get_json()
+
+    res = update_user(db, data)
+
+    return res
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
